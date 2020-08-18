92967ccd0dcc291dd50ddf218cb8b5cc08ace7d5
giogokul13 <63816836+giogokul13@users.noreply.github.com>
2020-08-18 13:31:17 +0530

    LoginPage added Initially

--- Coding-Challenge-4  NEW/Challenge4/src/app/app.module.ts
+++ Coding-Challenge-4  NEW/Challenge4/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
