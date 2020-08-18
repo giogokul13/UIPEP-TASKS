a8e8954b1f7953363fdd1a940bffb6e90e920293
giogokul13 <63816836+giogokul13@users.noreply.github.com>
2020-08-18 14:04:31 +0530

    Update app-routing.module.ts
    added routes for the pages to link the project with with different  components or html

--- Coding-Challenge-4  NEW/Challenge4/src/app/app-routing.module.ts
+++ Coding-Challenge-4  NEW/Challenge4/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegistrationComponent } from './registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'' ,component:HomepageComponent },
+  { path:'login' ,component:LoginPageComponent },
+  { path:'register' ,component:RegistrationComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
