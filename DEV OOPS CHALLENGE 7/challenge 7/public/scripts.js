6f60d05bc48dea001e297a0db7e831eb14faddb8
giogokul13 <63816836+giogokul13@users.noreply.github.com>
2020-10-01 22:54:50 +0530

    added multi video stream

--- DEV OOPS CHALLENGE 7/challenge 7/public/scripts.js
+++ DEV OOPS CHALLENGE 7/challenge 7/public/scripts.js
@@ -19,7 +19,23 @@ navigator.mediaDevices.getUserMedia({
 }).then( stream =>{
     myVideoStream = stream;
     addVideoStream(myVideo,stream);
+
+    peer.on('call', (call) => {
+        
+          call.answer(stream); // Answer the call with an A/V stream.
+          const video = document.createElement('video');
+          call.on('stream', userVideoStream => {
+            // Show stream in some video/canvas element.
+            addVideoStream(video,userVideoStream);
+          });
 });
+
+socket.on('user-connected', (userid)=>{
+    connectToNewUser(userid , stream);
+});
+
+});
+
 
 socket.emit('join-room',ROOM_ID);
 
@@ -27,13 +43,16 @@ peer.on('open' ,id =>{
     socket.emit('join-room',ROOM_ID,id);
 
 })
-socket.on('user-connected', (userid)=>{
-    connectToNewUser(userid);
-});
 
 
-const connectToNewUser = (userid) =>{
+
+const connectToNewUser = (userid, stream) =>{
     console.log( "new user connected " + userid);
+    const call = peer.call(userid,stream);
+    const video = document.createElement('video');
+    call.on('stream',userVideoStream =>{
+        addVideoStream(video, userVideoStream);
+    });
 }
 
 
